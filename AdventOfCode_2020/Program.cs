deb72052b6cfe66daed57e5e477ca840da3f1510
Scott Baldric <scottmbaldric@gmail.com>
2020-12-02 21:12:05 -0600

    Solution to day 1 puzzle 2.

--- AdventOfCode_2020/Program.cs
+++ AdventOfCode_2020/Program.cs
@@ -26,7 +26,7 @@ namespace AdventOfCode_2020
                 if (sum == 2020)
                 {
                     Console.WriteLine($"Day 1 Puzzle 1 Solution: {input[s] * input[e]}.");
-                    return;
+                    break;
                 }
                 else if (sum > 2020)
                 {
@@ -35,6 +35,38 @@ namespace AdventOfCode_2020
                 else
                 {
                     ++s;
+                }
+            }
+
+            for (int s1 = 0, s2 = 1, e = input.Length - 1; s2 < e;)
+            {
+                int sum = input[s1] + input[s2] + input[e];
+                if (sum == 2020)
+                {
+                    Console.WriteLine($"Day 1 Puzzle 2 Solution: {input[s1] * input[s2] * input[e]}.");
+                    break;
+                }
+                else if (sum > 2020)
+                {
+                    --e;
+                }
+                else if ((s2 - s1) == 1)
+                {
+                    ++s2;
+                }
+                else
+                {
+                    int s1Step = input[s1 + 1] - input[s1];
+                    int s2Step = input[s2 + 1] - input[s2];
+
+                    if (s1Step < s2Step)
+                    {
+                        ++s1;
+                    }
+                    else
+                    {
+                        ++s2;
+                    }
                 }
             }
         }
