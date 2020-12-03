2be6ea11a9c703102debf876088851382b8ac2cc
Scott Baldric <scottmbaldric@gmail.com>
2020-12-02 20:43:57 -0600

    Adding inputs.

--- AdventOfCode_2020/Program.cs
+++ AdventOfCode_2020/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AdventOfCode_2020
 {
     class Program
     {
+        static List<int> GetInput(string filename)
+        {
+            var path = Path.GetFullPath(filename);
+
+
+            return new List<int>();
+        }
+
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var tmp = GetInput("input.txt");
         }
     }
 }
