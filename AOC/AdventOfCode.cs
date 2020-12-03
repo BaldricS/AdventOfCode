fde1e097ff185f0e97f8b0622f66e69a78aeef65
Scott Baldric <scottmbaldric@gmail.com>
2020-12-02 21:30:48 -0600

    Making a little helper class.

---
+++ AOC/AdventOfCode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AOC
+{
+    public class AdventOfCode
+    {
+        private readonly int _day;
+        private readonly int _puzzle;
+
+        public AdventOfCode(int day, int puzzle)
+        {
+            _day = day;
+            _puzzle = puzzle;
+        }
+
+        public void Run<T, R>(Func<string, T> inputMap, Func<T[], R> solver)
+        {
+            var input = GetInput().Select(inputMap).ToArray();
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            var solution = solver(input);
+            sw.Stop();
+
+            Console.WriteLine($"=== Day {_day} Puzzle {_puzzle} ===");
+            Console.WriteLine($"Solution: {solution}");
+            Console.WriteLine($"Ellapsed Time: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine();
+        }
+
+        private string[] GetInput()
+        {
+            var filename = $"{_day}.txt";
+            return File.ReadAllLines($"inputs/{filename}");
+        }
+    }
+}
