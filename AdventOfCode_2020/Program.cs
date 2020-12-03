1a48661f18f9117ea184cf51199b5a6b71c037df
Scott Baldric <scottmbaldric@gmail.com>
2020-12-02 20:51:26 -0600

    Solution for first puzzle.

--- AdventOfCode_2020/Program.cs
+++ AdventOfCode_2020/Program.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode_2020
 {
     class Program
     {
-        static List<int> GetInput(string filename)
+        static int[] GetInput(string filename)
         {
             var path = Path.GetFullPath(filename);
+            var lines = File.ReadAllLines(path);
 
-
-            return new List<int>();
+            return lines.Select(int.Parse).ToArray();
         }
 
         static void Main()
         {
-            var tmp = GetInput("input.txt");
+            var input = GetInput("inputs/day1-1.txt");
+            Array.Sort(input);
+
+            for (int s = 0, e = input.Length - 1; s < e;)
+            {
+                int sum = input[s] + input[e];
+                if (sum == 2020)
+                {
+                    Console.WriteLine($"Day 1 Puzzle 1 Solution: {input[s] * input[e]}.");
+                    return;
+                }
+                else if (sum > 2020)
+                {
+                    --e;
+                }
+                else
+                {
+                    ++s;
+                }
+            }
         }
     }
 }
