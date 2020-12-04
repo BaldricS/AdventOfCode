2dd886fc35f7bf4e33cc7528b8ecea11f496b15e
Scott Baldric <scottmbaldric@gmail.com>
2020-12-03 23:35:14 -0600

    Solution to puzzle 2 day 4.

--- 2020_04/Program.cs
+++ 2020_04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using AOC;
 
@@ -15,7 +16,109 @@ namespace Day4
             return keys.All(k => passport.Contains(k, StringComparison.OrdinalIgnoreCase));
         }
 
-        public static int Solve1(string[] lines)
+        public static bool IsValidPassportStrict(string passport)
+        {
+            if (!IsValidPassport(passport))
+            {
+                return false;
+            }
+
+            var values = passport.Split(" ").ToDictionary(k => k.Split(":")[0], k => k.Split(":")[1]);
+
+            if (int.TryParse(values["byr"], out int year))
+            {
+                if (year < 1920 || year > 2020)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            if (int.TryParse(values["iyr"], out year))
+            {
+                if (year < 2010 || year > 2020)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            if (int.TryParse(values["eyr"], out year))
+            {
+                if (year < 2020 || year > 2030)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            var heightRegex = new Regex(@"^(\d+)(cm|in)$");
+            var matches = heightRegex.Match(values["hgt"]);
+            if (matches.Groups[2].Value == "in")
+            {
+                if (int.TryParse(matches.Groups[1].Value, out int height))
+                {
+                    if (height < 59 || height > 76)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (matches.Groups[2].Value == "cm")
+            {
+                if (int.TryParse(matches.Groups[1].Value, out int height))
+                {
+                    if (height < 150 || height > 193)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+
+            var hclRegex = new Regex(@"^#[0-9a-f]{6}$", RegexOptions.IgnoreCase);
+            if (!hclRegex.IsMatch(values["hcl"]))
+            {
+                return false;
+            }
+
+            var legalEyeColors = new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            if (!legalEyeColors.Any(e => values["ecl"].Equals(e)))
+            {
+                return false;
+            }
+
+            var idRegex = new Regex(@"^\d{9}$");
+            if (!idRegex.IsMatch(values["pid"]))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int Solve1(string[] lines, Func<string, bool> policy)
         {
             var passportPieces = new List<string>();
             int passports = 0;
@@ -24,7 +127,7 @@ namespace Day4
             {
                 if (lines[i].Length == 0)
                 {
-                    if (IsValidPassport(string.Join(" ", passportPieces)))
+                    if (policy(string.Join(" ", passportPieces)))
                     {
                         ++passports;
                     }
@@ -42,8 +145,11 @@ namespace Day4
 
         static void Main()
         {
-            var puzzle1 = new AdventOfCode(3, 1);
-            puzzle1.Run(Solve1);
+            var puzzle1 = new AdventOfCode(4, 1);
+            puzzle1.Run(lines => Solve1(lines, IsValidPassport));
+
+            var puzzle2 = new AdventOfCode(4, 2);
+            puzzle1.Run(lines => Solve1(lines, IsValidPassportStrict));
         }
     }
 }
