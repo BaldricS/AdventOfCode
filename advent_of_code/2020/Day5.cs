31a9152b089b48b321fb52a56f528ca7c8fe1039
Scott Baldric <ScottMBaldric@gmail.com>
2020-12-04 22:42:49 -0600

    Template for solution.

---
+++ advent_of_code/2020/Day5.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace AOC
+{
+    [AdventOfCode(2020, 5)]
+    public static class Day5_2020
+    {
+        [MapInput]
+        public static string MapLine(string line) => line;
+
+        [Solver(1)]
+        public static int Solve1(string[] input)
+        {
+            return 1;
+        }
+
+        [Solver(2)]
+        public static int Solve2(string[] input)
+        {
+            return 1;
+        }
+    }
+}
