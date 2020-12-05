9c380e374b531a9b0a9f6bbdf791ba9afa419d45
Scott Baldric <scottmbaldric@gmail.com>
2020-12-05 15:07:58 -0600

    Switching to record for password one.

--- advent_of_code/2020/Day2.cs
+++ advent_of_code/2020/Day2.cs
@@ -4,36 +4,30 @@ using System.Text.RegularExpressions;
 
 namespace AOC
 {
-    public struct Password
-    {
-        public int low;
-        public int high;
-        public char letter;
-        public string password;
-    }
+    public record Password(int Low, int High, char Letter, string Cleartext);
 
     [AdventOfCode(2020, 2)]
     public static class Day2_2020
     {
         static bool IsValidPasswordPolicy1(Password password) =>
-            password.password
-                .Count(c => c == password.letter)
-                .IsInRange(password.low, password.high);
+            password.Cleartext
+                .Count(c => c == password.Letter)
+                .IsInRange(password.Low, password.High);
 
         static bool IsValidPasswordPolicy2(Password password)
         {
-            var index1 = password.low - 1;
-            var index2 = password.high - 1;
-            var passLength = password.password.Length;
+            var index1 = password.Low - 1;
+            var index2 = password.High - 1;
+            var passLength = password.Cleartext.Length;
             if (passLength <= index1 || passLength <= index2)
             {
                 return false;
             }
 
-            var letter1 = password.password[index1];
-            var letter2 = password.password[index2];
+            var letter1 = password.Cleartext[index1];
+            var letter2 = password.Cleartext[index2];
 
-            return (letter1 == password.letter || letter2 == password.letter) && letter1 != letter2;
+            return (letter1 == password.Letter || letter2 == password.Letter) && letter1 != letter2;
         }
 
         [MapInput]
@@ -45,13 +39,12 @@ namespace AOC
             {
                 var match = regex.Match(line);
 
-                return new Password
-                {
-                    low = int.Parse(match.Groups[1].Value),
-                    high = int.Parse(match.Groups[2].Value),
-                    letter = match.Groups[3].Value[0],
-                    password = match.Groups[4].Value
-                };
+                return new Password(
+                    int.Parse(match.Groups[1].Value),
+                    int.Parse(match.Groups[2].Value),
+                    match.Groups[3].Value[0],
+                    match.Groups[4].Value
+                );
             }
 
             return lines.Select(MapLine);
