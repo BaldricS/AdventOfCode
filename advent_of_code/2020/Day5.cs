e03ef3d8061d0e9eb2eaf3ce71370721622d1657
Scott Baldric <ScottMBaldric@gmail.com>
2020-12-04 23:15:24 -0600

    Solving day 5 puzzle. Damn people are fast!

--- advent_of_code/2020/Day5.cs
+++ advent_of_code/2020/Day5.cs
@@ -1,23 +1,61 @@
 using System;
+using System.Linq;
 
 namespace AOC
 {
     [AdventOfCode(2020, 5)]
     public static class Day5_2020
     {
-        [MapInput]
-        public static string MapLine(string line) => line;
+        public static int SeatToRow(string seat, int low, int high, char lowC, char highC)
+        {
+            if (seat.Length == 1 && seat[0] == highC)
+            {
+                return high;
+            }
+            else if (seat.Length == 1 && seat[0] == lowC)
+            {
+                return low;
+            }
+
+            var mid = (high - low) / 2;
+            if (seat[0] == lowC)
+            {
+                return SeatToRow(seat.Substring(1), low, low + mid, lowC, highC);
+            }
+
+            return SeatToRow(seat.Substring(1), low + mid + 1, high, lowC, highC);
+        }
+
+        public static int GetSeatNumber(string line)
+        {
+            var rowStr = line.Substring(0, 7);
+            var colStr = line.Substring(7);
+
+            return SeatToRow(rowStr, 0, 127, 'F', 'B') * 8 + SeatToRow(colStr, 0, 7, 'L', 'R');
+        }
 
         [Solver(1)]
-        public static int Solve1(string[] input)
-        {
-            return 1;
-        }
+        public static int Solve1(string[] input) =>
+            input.Select(GetSeatNumber).Max();
 
         [Solver(2)]
         public static int Solve2(string[] input)
         {
-            return 1;
+            var allSeats = input.Select(GetSeatNumber).ToArray();
+            Array.Sort(allSeats);
+
+            for (int i = 1; i < allSeats.Length; ++i)
+            {
+                int prev = allSeats[i - 1];
+                int curr = allSeats[i];
+
+                if ((curr - prev) == 2)
+                {
+                    return curr - 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
