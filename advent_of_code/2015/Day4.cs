a2210f40b5f766d807bd336d91b3f956bb240db5
Scott Baldric <scottmbaldric@gmail.com>
2020-12-05 19:38:42 -0600

    2015, Day 4.

---
+++ advent_of_code/2015/Day4.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AOC
+{
+    [AdventOfCode(2015, 4)]
+    public static class Day4_2015
+    {
+        [Solver(1)]
+        public static int FirstMD5(IEnumerable<string> lines)
+        {
+            var key = lines.First();
+
+            int i = 0;
+            while (true)
+            {
+                var str = $"{key}{i}";
+                var bytes = Encoding.ASCII.GetBytes(str);
+                var hash = MD5.HashData(bytes);
+
+                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 8)
+                {
+                    return i;
+                }
+
+                ++i;
+            }
+        }
+
+        [Solver(2)]
+        public static int FirstMD52(IEnumerable<string> lines)
+        {
+            var key = lines.First();
+
+            int i = 0;
+            while (true)
+            {
+                var str = $"{key}{i}";
+                var bytes = Encoding.ASCII.GetBytes(str);
+                var hash = MD5.HashData(bytes);
+
+                if (hash[0] == 0 && hash[1] == 0 && hash[2] == 0)
+                {
+                    return i;
+                }
+
+                ++i;
+            }
+        }
+    }
+}
