ec6fe9eb7ddc93aaf2bc8da8f79363d2ca4fbc6a
Scott Baldric <scottmbaldric@gmail.com>
2020-12-06 00:11:25 -0600

    Cleaning up some of 2015 solutions.

--- advent_of_code/2015/Day5.cs
+++ advent_of_code/2015/Day5.cs
@@ -7,22 +7,14 @@ namespace AOC
     [AdventOfCode(2015, 5)]
     public static class Day5_2015
     {
-        public static bool IsNice(string line)
-        {
-            var vowels = new Regex(@"[aiueo].*[aiueo].*[aiueo]");
-            var doubleLetter = new Regex(@"(\w)\1");
-            var evilPairs = new Regex(@"ab|cd|pq|xy");
+        public static bool IsNice(string line) =>
+            Regex.IsMatch(line, @"[aeiou].*[aeiou].*[aeiou]") &&
+            Regex.IsMatch(line, @"(\w)\1") &&
+            !Regex.IsMatch(line, @"ab|cs|pq|xy");
 
-            return vowels.IsMatch(line) && doubleLetter.IsMatch(line) && !evilPairs.IsMatch(line);
-        }
-
-        public static bool IsActuallyNice(string line)
-        {
-            var doublePair = new Regex(@"(\w\w).*\1");
-            var huggingLetters = new Regex(@"(\w).\1");
-
-            return doublePair.IsMatch(line) && huggingLetters.IsMatch(line);
-        }
+        public static bool IsActuallyNice(string line) =>
+            Regex.IsMatch(line, @"(\w\w).*\1") &&
+            Regex.IsMatch(line, @"(\w).\1");
 
         [Solver(1)]
         public static int Solve1(IEnumerable<string> lines) => lines.Count(IsNice);
