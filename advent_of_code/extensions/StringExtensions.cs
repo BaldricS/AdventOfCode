5b02b5345ac90171e557a23ada590ab82c57ec0f
Scott Baldric <scottmbaldric@gmail.com>
2020-12-06 00:20:39 -0600

    Simplify conditional.

--- advent_of_code/extensions/StringExtensions.cs
+++ advent_of_code/extensions/StringExtensions.cs
@@ -3,8 +3,6 @@
     public static class StringExtensions
     {
         public static bool IsInRange(this string source, int min, int max) =>
-            int.TryParse(source, out int result)
-            ? result.IsInRange(min, max)
-            : false;
+            int.TryParse(source, out int result) && result.IsInRange(min, max);
     }
 }
