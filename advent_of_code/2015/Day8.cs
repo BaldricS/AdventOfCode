63acd847022eae8ba952b653a8d0f1d2e3ae2061
Scott Baldric <scottmbaldric@gmail.com>
2020-12-07 21:01:55 -0600

    2015, Day 8.

---
+++ advent_of_code/2015/Day8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC
+{
+    using ChallengeType = String;
+
+    [AdventOfCode(2015, 8)]
+    public static class Template
+    {
+        public static long GetCharacters(string line) =>
+            Regex.Replace(line, @"\\\\|\\""|\\x[a-f0-9]{2}", "_").Length - 2;
+
+        public static ChallengeType Encode(string line) =>
+            $"\"{line.Replace(@"\", @"\\").Replace("\"", @"\""")}\"";
+            
+
+        [Solver(1)]
+        public static long Solve1(IEnumerable<ChallengeType> input) =>
+            input.Sum(l => l.Length) - input.Sum(GetCharacters);
+
+        [Solver(2)]
+        public static long Solve2(IEnumerable<ChallengeType> input) =>
+            input.Select(Encode).Sum(l => l.Length) - input.Sum(l => l.Length);
+    }
+}
