2e1e835d17050e66b0157a2670c2077ddd3f4e34
Scott Baldric <scottmbaldric@gmail.com>
2020-12-12 12:08:51 -0600

    Minor cleanup.

--- advent_of_code/2020/Day12.cs
+++ advent_of_code/2020/Day12.cs
@@ -23,30 +23,25 @@ namespace AOC
         public static long Solve1(IEnumerable<ChallengeType> input)
         {
             var pos = new Pair(0, 0);
-            var movement = new[] { new Pair(1, 0), new Pair(0, -1), new Pair(-1, 0), new Pair(0, 1) };
+            var directions = new[] { new Pair(1, 0), new Pair(0, -1), new Pair(-1, 0), new Pair(0, 1) };
 
-            var E = 0;
-            var S = 1;
-            var W = 2;
-            var N = 3;
-
-            var facing = E;
+            var facing = 0;
 
             foreach (var action in input)
             {
                 var move = action.Action switch
                 {
-                    'N' => N,
-                    'E' => E,
-                    'S' => S,
-                    'W' => W,
+                    'E' => 0,
+                    'S' => 1,
+                    'W' => 2,
+                    'N' => 3,
                     'F' => facing,
                     _ => -1
                 };
 
                 if (move > -1)
                 {
-                    var dir = movement[move];
+                    var dir = directions[move];
                     pos = pos with { X = pos.X + dir.X * action.Amount, Y = pos.Y + dir.Y * action.Amount };
                 }
                 else
@@ -69,12 +64,7 @@ namespace AOC
         {
             var pos = new Pair(0, 0);
             var wpPos = new Pair(10, 1);
-            var movement = new[] { new Pair(1, 0), new Pair(0, -1), new Pair(-1, 0), new Pair(0, 1) };
-
-            var E = 0;
-            var S = 1;
-            var W = 2;
-            var N = 3;
+            var directions = new[] { new Pair(1, 0), new Pair(0, -1), new Pair(-1, 0), new Pair(0, 1) };
 
             foreach (var action in input)
             {
@@ -86,16 +76,16 @@ namespace AOC
 
                 var move = action.Action switch
                 {
-                    'N' => N,
-                    'E' => E,
-                    'S' => S,
-                    'W' => W,
+                    'E' => 0,
+                    'S' => 1,
+                    'W' => 2,
+                    'N' => 3,
                     _ => -1
                 };
 
                 if (move > -1)
                 {
-                    var dir = movement[move];
+                    var dir = directions[move];
                     wpPos = wpPos with { X = wpPos.X + dir.X * action.Amount, Y = wpPos.Y + dir.Y * action.Amount };
                 }
                 else
