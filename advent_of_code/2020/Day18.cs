31c03cc7d804c213250021f074c5ee43af47ce67
Scott Baldric <scottmbaldric@gmail.com>
2020-12-18 17:05:05 -0600

    Moving RPN and Postfix evaluator to its own class.

--- advent_of_code/2020/Day18.cs
+++ advent_of_code/2020/Day18.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace AOC
 {
@@ -17,167 +14,26 @@ namespace AOC
             return lines;
         }
 
-        public static long Process(string line)
-        {
-            var ops = new Stack<char>();
-            var output = new List<char>();
-
-            for (int i = 0; i < line.Length; ++i)
-            {
-                char c = line[i];
-                if (c >= '0' && c <= '9')
-                {
-                    output.Add(c);
-                }
-                else if (c == '+' || c == '*')
-                {
-                    while (ops.Any())
-                    {
-                        var o = ops.Pop();
-                        if (o == '(')
-                        {
-                            ops.Push(o);
-                            break;
-                        }
-
-                        output.Add(o);
-                    }
-
-                    ops.Push(c);
-                }
-                else if (c == '(')
-                {
-                    ops.Push(c);
-                }
-                else if (c == ')')
-                {
-                    while (ops.Any())
-                    {
-                        var o = ops.Pop();
-                        if (o == '(')
-                        {
-                            break;
-                        }
-
-                        output.Add(o);
-                    }
-                }
-            }
-
-            while (ops.Any())
-            {
-                output.Add(ops.Pop());
-            }
-
-            var values = new Stack<long>();
-            foreach (var c in output)
-            {
-                if (c >= '0' && c <= '9')
-                {
-                    values.Push(c - '0');
-                }
-                else
-                {
-                    long left = values.Pop();
-                    long right = values.Pop();
-
-                    long result = c switch
-                    {
-                        '+' => left + right,
-                        _ => left * right,
-                    };
-
-                    values.Push(result);
-                }
-            }
-
-            return values.Pop();
-        }
-
-        public static long Process2(string line)
-        {
-            var ops = new Stack<char>();
-            var output = new List<char>();
-
-            for (int i = 0; i < line.Length; ++i)
-            {
-                char c = line[i];
-                if (c >= '0' && c <= '9')
-                {
-                    output.Add(c);
-                }
-                else if (c == '+' || c == '*')
-                {
-                    if (c == '*' && ops.Any() && ops.Peek() == '+')
-                    {
-                        while (ops.Any())
-                        {
-                            var o = ops.Pop();
-                            if (o == '(')
-                            {
-                                ops.Push(o);
-                                break;
-                            }
-
-                            output.Add(o);
-                        }
-                    }
-
-                    ops.Push(c);
-                }
-                else if (c == '(')
-                {
-                    ops.Push(c);
-                }
-                else if (c == ')')
-                {
-                    while (ops.Any())
-                    {
-                        var o = ops.Pop();
-                        if (o == '(')
-                        {
-                            break;
-                        }
-
-                        output.Add(o);
-                    }
-                }
-            }
-
-            while (ops.Any())
-            {
-                output.Add(ops.Pop());
-            }
-
-            var values = new Stack<long>();
-            foreach (var c in output)
-            {
-                if (c >= '0' && c <= '9')
-                {
-                    values.Push(c - '0');
-                }
-                else
-                {
-                    long left = values.Pop();
-                    long right = values.Pop();
-
-                    long result = c switch
-                    {
-                        '+' => left + right,
-                        _ => left * right,
-                    };
-
-                    values.Push(result);
-                }
-            }
-
-            return values.Pop();
-        }
-
         [Solver(1)]
-        public static long Solve1(ChallengeType input) => input.Select(Process).Sum();
+        public static long Solve1(ChallengeType input)
+        {
+            var c = new Calculator();
+
+            c.AddOp('+', (a, b) => a + b);
+            c.AddOp('*', (a, b) => a * b);
+
+            return input.Select(l => c.Evaluate(l)).Sum();
+        }
 
         [Solver(2)]
-        public static long Solve2(ChallengeType input) => input.Select(Process2).Sum();
+        public static long Solve2(ChallengeType input)
+        {
+            var c = new Calculator();
+
+            c.AddOp('+', (a, b) => a + b, 1);
+            c.AddOp('*', (a, b) => a * b);
+
+            return input.Select(l => c.Evaluate(l)).Sum();
+        }
     }
 }
---
+++ advent_of_code/Utility/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC
+{
+    public class Calculator
+    {
+        private Dictionary<char, Func<long, long, long>> _ops;
+        private Dictionary<char, int> _precedence;
+        private bool _samePrecedence = true;
+
+        public Calculator()
+        {
+            _ops = new Dictionary<char, Func<long, long, long>>();
+            _precedence = new Dictionary<char, int>();
+        }
+
+        public long Evaluate(string expression) => Evaluate(ToPostfix(expression));
+
+        public void AddOp(char op, Func<long, long, long> action, int precedence = 0)
+        {
+            _ops[op] = action;
+            _precedence[op] = precedence;
+            _samePrecedence = _samePrecedence && precedence == 0;
+        }
+
+        private long Evaluate(List<char> postfix)
+        {
+            var results = new Stack<long>();
+
+            foreach (var c in postfix)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    results.Push(c - '0');
+                }
+                else
+                {
+                    long left = results.Pop();
+                    long right = results.Pop();
+
+                    results.Push(_ops[c](left, right));
+                }
+            }
+
+            return results.Pop();
+        }
+
+        private List<char> ToPostfix(string expression)
+        {
+            var postfix = new List<char>();
+            var ops = new Stack<char>();
+
+            void Move()
+            {
+                while (ops.Any() && ops.Peek() != '(')
+                {
+                    postfix.Add(ops.Pop());
+                }
+            }
+
+            foreach (var c in expression)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    postfix.Add(c);
+                }
+                else if (_ops.ContainsKey(c))
+                {
+                    while (ops.Any() && ops.Peek() != '(' && _precedence[c] <= _precedence[ops.Peek()])
+                    {
+                        postfix.Add(ops.Pop());
+                    }
+
+                    ops.Push(c);
+                }
+                else if (c == '(')
+                {
+                    ops.Push(c);
+                }
+                else if (c == ')')
+                {
+                    Move();
+                    ops.Pop();
+                }
+            }
+
+            Move();
+
+            return postfix;
+        }
+    }
+}
