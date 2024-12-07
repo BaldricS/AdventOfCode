f9b5e997e73e420bf6b671cc0e0312b4343eb049
Scott Baldric <ScottMBaldric@gmail.com>
2024-12-07 00:25:45 -0800

    Refactor to take array of ops

--- advent_of_code/2024/Day7.cs
+++ advent_of_code/2024/Day7.cs
@@ -16,11 +16,17 @@ namespace AOC
 
         [Solver(1)]
         public static long Solve1(IEnumerable<Equation20247> input) =>
-            input.Where(eq => Evaluate(eq.Result, eq.Operands[0], 1, eq.Operands))
+            input.Where(eq => Evaluate(eq.Result, eq.Operands[0], 1, eq.Operands, [Add, Mul]))
                 .Select(eq => eq.Result)
                 .Sum();
 
-        public static bool Evaluate(long target, long a, int idx, long[] rest)
+        public static long Add(long a, long b) => a + b;
+
+        public static long Mul(long a, long b) => a * b;
+
+        public static long Concat(long a, long b) => long.Parse($"{a}{b}");
+
+        public static bool Evaluate(long target, long a, int idx, long[] rest, Func<long, long, long>[] ops)
         {
             if (idx == rest.Length)
             {
@@ -28,31 +34,13 @@ namespace AOC
             }
 
             long b = rest[idx++];
-            long tempAdd = a + b;
-            long tempMul = a * b;
-
-            return Evaluate(target, tempAdd, idx, rest) || Evaluate(target, tempMul, idx, rest);
+            return ops.Any(op => Evaluate(target, op(a, b), idx, rest, ops));
         }
 
         [Solver(2)]
         public static long Solve2(IEnumerable<Equation20247> input) =>
-            input.Where(eq => Evaluate2(eq.Result, eq.Operands[0], 1, eq.Operands))
+            input.Where(eq => Evaluate(eq.Result, eq.Operands[0], 1, eq.Operands, [Add, Mul, Concat]))
                 .Select(eq => eq.Result)
                 .Sum();
-
-        public static bool Evaluate2(long target, long a, int idx, long[] rest)
-        {
-            if (idx == rest.Length)
-            {
-                return target == a;
-            }
-
-            long b = rest[idx++];
-            long tempAdd = a + b;
-            long tempMul = a * b;
-            long tempConcat = long.Parse($"{a}{b}");
-
-            return Evaluate2(target, tempAdd, idx, rest) || Evaluate2(target, tempMul, idx, rest) || Evaluate2(target, tempConcat, idx, rest);
-        }
     }
 }
