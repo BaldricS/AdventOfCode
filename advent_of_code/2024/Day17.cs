e7eed4b122272441639fde6a81ea5681a2a23f55
Scott Baldric <ScottMBaldric@gmail.com>
2024-12-18 15:57:20 -0800

    Day 17, 2024, part 2

--- advent_of_code/2024/Day17.cs
+++ advent_of_code/2024/Day17.cs
@@ -103,61 +103,123 @@ namespace AOC
         }
 
         [Solver(2)]
-        public static string Solve2(Input202417 input)
+        public static long Solve2(Input202417 input)
         {
-            long[] wanted = [2,4,1,1,7,5,0,3,1,4,4,4,5,5,3,0];
+            int[] wanted = [2,4,1,1,7,5,0,3,1,4,4,4,5,5,3,0];
+            int?[] bits = Enumerable.Range(0, 64).Select(_ => (int?)null).ToArray();
 
-            for (int a = 0; a < 7; ++a)
-            for (int b = 0; b < 7; ++b)
-            for (int c = 0; c < 7; ++c)
-            for (int d = 0; d < 7; ++d)
-            for (int e = 0; e < 7; ++e)
-            for (int f = 0; f < 7; ++f)
-            for (int g = 0; g < 7; ++g)
-            for (int h = 0; h < 7; ++h)
-            for (int i = 0; i < 7; ++i)
-            for (int j = 0; j < 7; ++j)
-            for (int k = 0; k < 7; ++k)
-            for (int l = 0; l < 7; ++l)
-            for (int m = 0; m < 7; ++m)
-            for (int n = 0; n < 7; ++n)
-            for (int o = 0; o < 7; ++o)
-            for (int p = 0; p < 7; ++p)
+            HashSet<long> validNumbers = [];
+
+            FindNumbers(0, wanted, bits, validNumbers);
+
+            Console.WriteLine(validNumbers.Count);
+            foreach (var n in validNumbers)
             {
-                long maybe = PiecesToNum([p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a]);
-
-                bool found = true;
-                long tmp = maybe;
-                foreach (var w in wanted)
-                {
-                    if (CalculateStep(tmp) != w)
-                    {
-                        found = false;
-                        break;
-                    }
-                    tmp >>= 3;
-                }
-
-                if (found)
-                {
-                    return $"{maybe}";
-                }
+                Console.WriteLine(n);
             }
 
-            input.RegA = 6175130917;
-            return RunProgram(input);
+            Console.WriteLine(validNumbers.Max());
+
+            return validNumbers.Min();
         }
 
-        public static long PiecesToNum(int[] pieces)
+        public static void FindNumbers(int wIdx, int[] wanted, int?[] bits, HashSet<long> validNumbers)
         {
-            long n = 0;
-            foreach (var p in pieces)
+            // Done writing
+            if (wIdx == wanted.Length)
             {
-                n <<= 3;
-                n |= (long)p;
+                validNumbers.Add(bits.Aggregate(0L, (n, b) => (n << 1) | (b ?? 0)));
+                return;
             }
 
-            return n;
+            int[] b4Cases = [0b101, 0b100, 0b111, 0b110, 0b001, 0b000, 0b011, 0b010];
+
+            int w = wanted[wIdx];
+            int bOffset = bits.Length - 3 * (wIdx + 1);
+
+            foreach (var b4 in b4Cases)
+            {
+                int cLast3 = w ^ b4;
+                int?[] copiedBits = [.. bits];
+
+                if (WriteBits(cLast3, b4, copiedBits, bOffset))
+                {
+                    FindNumbers(wIdx + 1, wanted, copiedBits, validNumbers);
+                }
+            }
+        }
+
+        public static bool WriteBits(int c, int b4Case, int?[] bits, int startOfb)
+        {
+            if (b4Case == 0b101)
+            {
+                return TryWriteLoop(bits, 0, startOfb) && TryWriteLoop(bits, c, startOfb - 1);
+            }
+
+            if (b4Case == 0b100)
+            {
+                return TryWriteLoop(bits, 0b001, startOfb) && TryWriteLoop(bits, c, startOfb);
+            }
+
+            if (b4Case == 0b111)
+            {
+                return TryWriteLoop(bits, 0b010, startOfb) && TryWriteLoop(bits, c, startOfb - 3);
+            }
+
+            if (b4Case == 0b110)
+            {
+                return TryWriteLoop(bits, 0b011, startOfb) && TryWriteLoop(bits, c, startOfb - 2);
+            }
+
+            if (b4Case == 0b001)
+            {
+                return TryWriteLoop(bits, 0b100, startOfb) && TryWriteLoop(bits, c, startOfb - 5);
+            }
+
+            if (b4Case == 0b000)
+            {
+                return TryWriteLoop(bits, 0b101, startOfb) && TryWriteLoop(bits, c, startOfb - 4);
+            }
+
+            if (b4Case == 0b011)
+            {
+                return TryWriteLoop(bits, 0b110, startOfb) && TryWriteLoop(bits, c, startOfb - 7);
+            }
+
+            if (b4Case == 0b010)
+            {
+                return TryWriteLoop(bits, 0b111, startOfb) && TryWriteLoop(bits, c, startOfb - 6);
+            }
+
+            throw new Exception($"case: {b4Case}");
+        }
+
+        public static bool TryWriteLoop(int?[] bits, int value, int idx)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                if (!TryWrite(bits, (value >> (2 - i)) & 1, idx + i))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool TryWrite(int?[] bits, int value, int idx)
+        {
+            if (idx < 0 || idx >= bits.Length)
+            {
+                return false;
+            }
+
+            if (!bits[idx].HasValue)
+            {
+                bits[idx] = value;
+            }
+
+            return bits[idx].Value == value;
         }
 
 /*
@@ -175,7 +237,7 @@ namespace AOC
             long B = A % 8;
             B ^= 1;
 
-            long C = (long)(A / Math.Pow(2, B));
+            long C = A >> (int)B;
             B ^= 4;
             B ^= C;
             return B % 8;
